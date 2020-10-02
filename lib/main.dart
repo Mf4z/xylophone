e2ab583db8a3dbd663dd5ac4e0ff3f870e4aebfa
Marwan Mai <emef4z@gmail.com>
2020-10-02 21:51:50 +0100

    Add use method playSound() function to not repeat codes

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,69 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    // call this method when desired
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-          child: FlatButton(
+            child: Column(
+          children: <Widget>[
+            FlatButton(
               onPressed: () {
-                final player = AudioCache();
-                // call this method when desired
-                player.play('note1.wav');
+                playSound(1);
               },
-              child: Text('Click Here')),
+              color: Colors.red,
+            ),
+            FlatButton(
+              onPressed: () {
+                // call this method when desired
+                playSound(2);
+              },
+              child: null,
+              color: Colors.orange,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(3);
+              },
+              child: null,
+              color: Colors.green,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(4);
+              },
+              child: null,
+              color: Colors.teal,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(5);
+              },
+              child: null,
+              color: Colors.teal,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(6);
+              },
+              child: null,
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(7);
+              },
+              child: null,
+              color: Colors.purple,
+            ),
+          ],
         )),
       ),
     );
